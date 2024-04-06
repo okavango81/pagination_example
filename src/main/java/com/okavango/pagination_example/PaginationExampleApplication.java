ec562a43032a3bbd7c96a4f70efbb48c2f0f0970
okavango81 <eguilhermeleitedev81@gmail.com>
2024-04-05 21:12:31 -0300

    created project

---
+++ src/main/java/com/okavango/pagination_example/PaginationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.okavango.pagination_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaginationExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/okavango/pagination_example/PaginationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okavango.pagination_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
