a36484cfac37eed06f5a52ecf81a79d74ed1e210
okavango81 <eguilhermeleitedev81@gmail.com>
2024-04-05 21:25:18 -0300

    injection to bean validation and creating a class of example

---
+++ src/main/java/com/okavango/pagination_example/entity/Student.java
@@ -0,0 +1,41 @@
+package com.okavango.pagination_example.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank
+    @Size(min = 3, message = "must have be minimum 3 characters")
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
